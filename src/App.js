fa11d75f6a97b5d093b6ea993666dd72c0b2da60
Chikusonu <druva186@gmail.com>
2021-12-12 18:56:09 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
       <h1>Hey world!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
-    
-
-      
   );
 }
